b264413a6802b21db72c82189376c35608962405
Sina Kian <70090343+kiansina@users.noreply.github.com>
2022-08-26 10:36:51 +0200

    Update i-law.py

--- i-law.py
+++ i-law.py
@@ -130,18 +130,26 @@ def tim():
     st.session_state["t0"] = time.time()
     return st.session_state["t0"]
 
+@st.experimental_singleton
+def use():
+    return st.session_state["username"]
+
 if check_password():
+    st.session_state["username"]=use()
     @st.cache(allow_output_mutation=True)
     def get_data():
         return []
-    Username=st.text_input("Username:")
+    st.title('Benvenuto al Bordo Gentile {} 🚀'.format(st.session_state["username"]))
+    st.title('Controlla lo stato della sua esame per favore')
     if st.button("check"):
-        if Username in df['Username'].to_list():
+        if st.session_state["username"] in df['Username'].to_list():
             st.session_state['usercheck']=False
             st.write('l\'esame gia registrato 😊.')
-        elif Username not in st.secrets['passwords'].keys():
+        elif st.session_state["username"] not in st.secrets['passwords'].keys():
             st.write('😕 User not known')
         else:
+            st.write('L\'esame è iniziato, ricorda che ha 5 minuti. Dopo 5 minuti ancora si può registerare l\'esame, pero non si può superare')
+            st.write('prepara il suo tempo')
             st.session_state['usercheck']=True
             st.session_state['st']=True
             st.session_state["t0"]=tim()
@@ -156,9 +164,9 @@ if check_password():
             Qd=st.radio("4)    "+questions[str(st.session_state["rn"][3])],st.session_state["cho3"],horizontal=False)
             Qe=st.radio("5)    "+questions[str(st.session_state["rn"][4])],st.session_state["cho4"],horizontal=False)
             if st.button("Submit"):
-                get_data().append({"Username":Username,"Nome": Nome,"Cognome":Cognome, "Livello_sodisfazione": sodisfazione, "q1": Qa, "q2": Qb, "q3": Qc, "q4": Qd, "q5": Qe, "time":time.time()-st.session_state["t0"]})
+                get_data().append({"Username":st.session_state["username"],"Nome": Nome,"Cognome":Cognome, "Livello_sodisfazione": sodisfazione, "q1": Qa, "q2": Qb, "q3": Qc, "q4": Qd, "q5": Qe, "time":time.time()-st.session_state["t0"]})
                 A=pd.DataFrame(get_data())
-                st.session_state["B"]=pd.DataFrame({"Username":Username,"Nome": Nome,"Cognome":Cognome, "Livello_sodisfazione": sodisfazione, "q1": st.session_state["che0"][st.session_state["cho0"].index(Qa)], "q2": st.session_state["che1"][st.session_state["cho1"].index(Qb)], "q3": st.session_state["che2"][st.session_state["cho2"].index(Qc)], "q4": st.session_state["che3"][st.session_state["cho3"].index(Qd)], "q5": st.session_state["che4"][st.session_state["cho4"].index(Qe)], "time":(time.time()-st.session_state["t0"])//60},index=[0])
+                st.session_state["B"]=pd.DataFrame({"Username":st.session_state["username"],"Nome": Nome,"Cognome":Cognome, "Livello_sodisfazione": sodisfazione, "q1": st.session_state["che0"][st.session_state["cho0"].index(Qa)], "q2": st.session_state["che1"][st.session_state["cho1"].index(Qb)], "q3": st.session_state["che2"][st.session_state["cho2"].index(Qc)], "q4": st.session_state["che3"][st.session_state["cho3"].index(Qd)], "q5": st.session_state["che4"][st.session_state["cho4"].index(Qe)], "time":(time.time()-st.session_state["t0"])//60},index=[0])
                 st.write(A)
             st.title('Se Lei è sicuro da chiudere l\'esamae, premi conferma')
             if st.button("Confirm"):
